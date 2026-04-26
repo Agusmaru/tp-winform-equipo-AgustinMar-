ba549b4fb5e2756e3f190a6f794032d0ffec2f7c
Agusmaru <97960364+Agusmaru@users.noreply.github.com>
2026-04-25 21:48:21 -0300

    Add files via upload
    Se agrega capa de acceso a datos

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class AccesoDatos : IDisposable
+    {
+        private readonly SqlConnection conexion;
+        private readonly SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            string cadenaConexion = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+            conexion = new SqlConnection(cadenaConexion);
+            comando = new SqlCommand();
+
+        }
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = consulta;
+            comando.Parameters.Clear();
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor ?? DBNull.Value);
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            if (conexion.State != ConnectionState.Open)
+                conexion.Open();
+
+            lector = comando.ExecuteReader();
+        }
+
+        public int ejecutarAccion()
+        {
+            comando.Connection = conexion;
+
+            if (conexion.State != ConnectionState.Open)
+                conexion.Open();
+
+            return comando.ExecuteNonQuery();
+        }
+
+        public object ejecutarScalar()
+        {
+            comando.Connection = conexion;
+
+            if (conexion.State != ConnectionState.Open)
+                conexion.Open();
+
+            return comando.ExecuteScalar();
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null && !lector.IsClosed)
+                lector.Close();
+
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+
+        public void Dispose()
+        {
+            cerrarConexion();
+            comando.Dispose();
+            conexion.Dispose();
+        }
+
+
+
+
+    }
+
+}
